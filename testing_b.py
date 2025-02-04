756c1778ee10174c204719eea4e84c32f4f8173e
Alessio <alebre16@gmail.com>
2025-02-03 21:29:02 -0500

    update

--- testing_b.py
+++ testing_b.py
@@ -18,28 +18,28 @@ df_global.index = pd.to_datetime(df_global.index, format="%Y%m")  # clean the in
 
 # constants
 start_date = "2019-12-01"  # 5 years worth of data (60 entries)
-N = 48
 risk_free = 0.004  # taken from Kenneth French Library
 df_global = df_global.loc[df_global.index >= start_date, :]  # select last 5 years
 df = df_global.loc[:, ["Whlsl", "Fin  ", "Util ", "Cnstr", "Hlth "]]  # select 5 assets to work with
 
-N = 5
+N = 6
+rf = pd.DataFrame(risk_free, index=df.index, columns=["risk-free"])
+df_rf = pd.concat([rf, df], axis=1)
+
 cov = df.cov()
 mean = df.mean(axis=0)
 std = np.sqrt(np.diag(cov))
 
 # global mean variance portfolio
-init_w = np.repeat(1 / N, N)
-target_returns = np.linspace(0.02, 0.04, 100)
-x_no_rf = []
-y_no_rf = []
+target_returns = np.linspace(-8, 8, 202)
+x_rf = []
+y_rf = []
 for ret in target_returns:
-    obj_val, weights = optimization.minimize_variance_gurobi(mean, cov, ret, long_only= True)
-    # print(f'weights: {weights}')
-    # print(f'obj_val: {obj_val}')
+    obj_val, weights = optimization.minimize_variance_gurobi(mean, cov, ret, risk_free_asset= True, long_only= True)
+    print(obj_val)
     if obj_val is not None:
-        x_no_rf.append(obj_val)
-        y_no_rf.append(mean @ weights)
+        x_rf.append(np.sqrt(obj_val))
+        y_rf.append(mean @ weights)
 
 # plot
-plotting.mean_var_locus(x_no_rf, y_no_rf, std, mean, 'Mean Variance Locus (No Risk Free Asset & Short Sale Constraint)')
+plotting.mean_var_locus(x_rf, y_rf, std, mean, 'Mean Variance Locus (With Risk Free Asset & Short Sale Constraint)')
